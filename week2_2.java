049e1402212f7b407f2cfb581d2819b52e1cf046
Varshithays <86124941+Varshithays@users.noreply.github.com>
2022-02-13 00:02:17 +0530

    Create week2_2.java

---
+++ week2_2.java
@@ -0,0 +1,29 @@
+/*Complete the code segment to call the method  print() of class given class Printer to print the following.
+--------------------------------
+  Hi! I am class STUDENT
+Hi! I class SCHOOL.
+--------------------------------
+NOTE: Don't provide any INPUT in Sample Test Cases*/
+
+
+// This is the class named Printer
+class Printer { 
+    // This are the methods in class Printer
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+    public void print(String s) { 
+		System.out.println(s); 
+    } 
+} 
+
+public class Question22{ 
+    public static void main(String[] args) {    
+
+// Create an object of class Printer
+	Printer p = new Printer();
+// Call 'print()' methods for desired output
+	p.print("Hi! I am class STUDENT");
+	p.print();
+    }
+}
